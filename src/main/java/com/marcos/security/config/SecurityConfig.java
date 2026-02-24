e6e73be02cf3a5052fb5d884830af3e974d5ac5e
Marcos <marcos.yuriro@gmail.com>
2026-02-24 17:14:49 -0300

    feat: adds the securityFilterChain method

--- src/main/java/com/marcos/security/config/SecurityConfig.java
+++ src/main/java/com/marcos/security/config/SecurityConfig.java
@@ -1,5 +1,31 @@
 package com.marcos.security.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
+	@Bean
+	private SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+		http
+			.authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated())
+			//Define que toda requisição tem que ser autenticada.
+			.csrf(csrf -> csrf.disable())
+			//Recomendado apenas localmente/para testes
+			.oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+			//Utilizará as configurações padrões do JWT
+			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+			//Define a sessão como 'sem estado'
+		
+		return http.build();
+		//http.build joga uma exception que pode ser tratada na declaração do método
+	}
 }
